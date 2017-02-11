6ca8c46d832a01728a322135472536d29d5d6e0d
Sebastian Quack <sebastianquack@gmail.com>
2017-02-11 15:07:40 +0100

    add basic recording

--- src/pages/new/new.ts
+++ src/pages/new/new.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { MediaCapture, MediaFile, CaptureError, CaptureVideoOptions } from 'ionic-native';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-new',
@@ -8,8 +9,32 @@ import { NavController } from 'ionic-angular';
 })
 export class NewPage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
   }
+
+  doCapture() {
+	let options: CaptureVideoOptions = { limit: 1 };
+	MediaCapture.captureVideo(options)
+  		.then(
+    	(data: MediaFile[]) => {
+    		var fileName = data[0].fullPath;
+			let alert = this.alertCtrl.create({
+      			title: 'Recording complete',
+      			subTitle: fileName,
+      			buttons: ['OK']
+    		});
+    		alert.present();
+    	},
+    	(err: CaptureError) => {
+    		let alert = this.alertCtrl.create({
+      			title: 'Recording error',
+      			subTitle: err.toString(),
+      			buttons: ['OK']
+    		});
+    		alert.present();
+    	}
+  	);
+  }
+
 
 }
